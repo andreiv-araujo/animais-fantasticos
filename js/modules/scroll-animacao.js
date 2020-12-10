318be26e6717e5dbb98ef51eb8ce6252ed069003
Andreiv Araújo <andreiv0712@gmail.com>
2020-12-10 15:37:44 -0300

    primeiro commit

---
+++ js/modules/scroll-animacao.js
@@ -0,0 +1,21 @@
+export default function initAnimateScroll() {
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  const windowHalf = window.innerHeight * 0.6;
+
+  function animateScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - windowHalf < 0;
+      if (isSectionVisible) {
+        section.classList.add('ativo');
+      } else if (section.classList.contains('ativo')) {
+        section.classList.remove('ativo');
+      }
+    });
+  }
+
+  if (sections.length) {
+    animateScroll();
+    window.addEventListener('scroll', animateScroll);
+  }
+}
