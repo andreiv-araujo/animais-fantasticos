0d6ac99358c1c9b63e9aa30f99d57299d95637bb
Andreiv Araújo <andreiv0712@gmail.com>
2020-12-13 14:48:04 -0300

    refatoracao com class do scroll anima

---
+++ js/modules/scroll-anima.js
@@ -0,0 +1,25 @@
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowHalf = window.innerHeight * 0.6;
+
+    this.animateScroll = this.animateScroll.bind(this);
+  }
+
+  animateScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - this.windowHalf < 0;
+      if (isSectionVisible) {
+        section.classList.add('ativo');
+      } else if (section.classList.contains('ativo')) {
+        section.classList.remove('ativo');
+      }
+    });
+  }
+
+  init() {
+    this.animateScroll();
+    window.addEventListener('scroll', this.animateScroll);
+  }
+}
