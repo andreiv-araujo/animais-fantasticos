cdcad96f99bc4cc9aa964b54d1b31089d5abf9b8
Andreiv Araújo <andreiv0712@gmail.com>
2020-12-20 23:08:17 -0300

    adicionado slide de fotos

--- js/script.js
+++ js/script.js
@@ -5,28 +5,22 @@ import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import DropdownMenu from './modules/dropdown-menu.js';
 import MenuMobile from './modules/menu-mobile.js';
-import Funcionamento from './modules/funcionamento.js';
+import Functionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 import ScrollAnima from './modules/scroll-anima.js';
+import SlideNav from './modules/slide.js';
 
-const scroolSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
-scroolSuave.init();
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
 
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
-const tabnav = new TabNav(
-  '[data-tab="menu"] li',
-  '[data-tab="content"] section'
-);
-tabnav.init();
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+tabNav.init();
 
-const modal = new Modal(
-  '[data-modal="abrir"]',
-  '[data-modal="fechar"]',
-  '[data-modal="container"]'
-);
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
 
 const tooltip = new Tooltip('[data-tooltip]');
@@ -41,9 +35,13 @@ dropdownMenu.init();
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
-const funcionamento = new Funcionamento('[data-semana]', 'aberto');
+const funcionamento = new Functionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
 fetchAnimais('./animaisapi.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+slide.addControl('.custom-controls');
